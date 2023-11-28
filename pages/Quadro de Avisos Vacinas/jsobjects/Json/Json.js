7858fca9bd31a8e4009dd1a1f3a78f6490854735
Kaua <kaua271203@gmail.com>
2023-11-28 21:43:36 +0000

    Nenhum aviso para hoje

--- pages/Quadro de Avisos Vacinas/jsobjects/Json/Json.js
+++ pages/Quadro de Avisos Vacinas/jsobjects/Json/Json.js
@@ -1,7 +1,17 @@
 export default {
 	pegaJson(){
 	//let aux = listaIdentificada;
-	let ajuda = Read_vacinas_do_dia.data.map((objeto) => objeto);
-	return ajuda
+	let answer = Read_vacinas_do_dia.data.map((objeto) => objeto);
+	if(answer.length==0){
+		return Array([])
+	}
+	return answer
+	},
+	pegaDados(content){
+		//let content = [{"tipo_vacina": 'V10', "nome_de_registro": 'Luna'}]
+		if (content.length == 0){
+			return "Não há avisos"
+		}
+		return 'Vacina: ' + content["tipo_vacina"] + '\nPara o cão: ' + content["nome_de_registro"]
 	}
 }
